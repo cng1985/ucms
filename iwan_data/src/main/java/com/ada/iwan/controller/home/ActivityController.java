d82e898a50446e8b8319854998f33e454af66190
ada.young <cng1985@gmail.com>
2016-03-10 10:47:03 +0800

    新增分页

--- iwan_data/src/main/java/com/ada/iwan/controller/home/ActivityController.java
+++ iwan_data/src/main/java/com/ada/iwan/controller/home/ActivityController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ada.activity.page.ActivityPage;
 import com.ada.activity.service.ActivityCatalogService;
 import com.ada.activity.service.ActivityService;
 import com.ada.iwan.controller.BaseController;
@@ -41,9 +42,14 @@ public class ActivityController extends BaseController{
 	public String catalog(@RequestParam(value = "id", required = true, defaultValue = "1") int id,
 			@RequestParam(value = "curpage", required = true, defaultValue = "1") int curpage,
 			@RequestParam(value = "pagesize", required = true, defaultValue = "20") int pagesize, Model model) {
-		model.addAttribute("activitys", activityService.pageByCatalog(id,curpage, pagesize).getList());
+		
+		ActivityPage page = activityService.pageByCatalog(id,curpage, pagesize);
+		model.addAttribute("activitys", page.getList());
 		model.addAttribute("catalogs", articleCatalogService.findChild(1));
+		model.addAttribute("page", page);
 		model.addAttribute("id", id);
+		model.addAttribute("curpage", curpage);
+		model.addAttribute("pagesize", pagesize);
 		return getView("activity/catalog");
 	}
 }
--- iwan_data/src/main/java/com/ada/iwan/controller/home/ArticleController.java
+++ iwan_data/src/main/java/com/ada/iwan/controller/home/ArticleController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ada.article.page.ArticlePage;
 import com.ada.article.service.ArticleCatalogService;
 import com.ada.article.service.ArticleService;
 import com.ada.iwan.controller.BaseController;
@@ -40,9 +41,14 @@ public class ArticleController extends BaseController {
 	public String catalog(@RequestParam(value = "id", required = true, defaultValue = "1") int id,
 			@RequestParam(value = "curpage", required = true, defaultValue = "1") int curpage,
 			@RequestParam(value = "pagesize", required = true, defaultValue = "20") int pagesize, Model model) {
-		model.addAttribute("articles", articleService.pageByCatalog(id,curpage, pagesize).getList());
+		
+		ArticlePage page = articleService.pageByCatalog(id,curpage, pagesize);
+		model.addAttribute("articles", page.getList());
 		model.addAttribute("catalogs", articleCatalogService.findChild(1));
+		model.addAttribute("page", page);
 		model.addAttribute("id", id);
+		model.addAttribute("curpage", curpage);
+		model.addAttribute("pagesize", pagesize);
 		return getView("article/catalog");
 	}
 }
