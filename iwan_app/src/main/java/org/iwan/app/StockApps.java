33b2903623e9d3fc51f281a23cb430b3566aaf21
ada.young <cng1985@gmail.com>
2016-03-12 23:52:57 +0800

    x

--- iwan_app/src/main/java/org/iwan/app/StockApps.java
+++ iwan_app/src/main/java/org/iwan/app/StockApps.java
@@ -8,7 +8,7 @@ public class StockApps {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for (int i = 1; i < 2788; i++) {
-			HttpRequest httpRequest = HttpRequest.get("http://192.168.199.171:8080/iwan_web/stock/view/"+i+".htm");
+			HttpRequest httpRequest = HttpRequest.get("http://www.yichisancun.com/stock/view/"+i+".htm");
 		    HttpResponse response = httpRequest.send();
 		}
 		   
