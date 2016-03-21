7c7ce327009511a8377d1e26545f405ef55fe06e
ada.young <cng1985@gmail.com>
2016-03-21 23:26:47 +0800

    修改问答分类后台

--- iwan_data/src/main/java/com/ada/iwan/controller/admin/QuestionCatalogAction.java
+++ iwan_data/src/main/java/com/ada/iwan/controller/admin/QuestionCatalogAction.java
@@ -1,7 +1,5 @@
 package com.ada.iwan.controller.admin;
 
-
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -10,27 +8,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ada.data.page.Filter;
 import com.ada.data.page.Order;
+import com.ada.data.page.Page;
 import com.ada.data.page.Pageable;
 import com.ada.question.entity.QuestionCatalog;
 import com.ada.question.service.QuestionCatalogService;
+
 @Controller
 public class QuestionCatalogAction {
 	private static final Logger log = LoggerFactory.getLogger(QuestionCatalogAction.class);
 
 	@RequestMapping("/admin/questioncatalog/view_list")
-	public String list(Pageable pageable, HttpServletRequest request, ModelMap model) {
-	
-		if (pageable==null) {
-			pageable=new Pageable();
+	public String list(@RequestParam(value = "id", required = true, defaultValue = "1") int id, Pageable pageable,
+			HttpServletRequest request, ModelMap model) {
+
+		if (pageable == null) {
+			pageable = new Pageable();
 		}
-		if (pageable.getOrders()==null||pageable.getOrders().size()==0) {
+		if (pageable.getOrders() == null || pageable.getOrders().size() == 0) {
 			pageable.getOrders().add(Order.desc("id"));
 		}
-//		Page<QuestionCatalog> pagination = manager.findPage(pageable);
-//		model.addAttribute("list", pagination.getContent());
-//		model.addAttribute("page", pagination);
+		pageable.getFilters().add(Filter.eq("parent.id", id));
+		model.addAttribute("id", id);
+		model.addAttribute("tops", manager.findTop(id));
+		 Page<QuestionCatalog> pagination = manager.findPage(pageable);
+		 model.addAttribute("list", pagination.getContent());
+		model.addAttribute("page", pagination);
 		return "admin/questioncatalog/list";
 	}
 
@@ -40,40 +46,68 @@ public class QuestionCatalogAction {
 	}
 
 	@RequestMapping("/admin/questioncatalog/view_edit")
-	public String edit(Integer id, Integer pageNo, HttpServletRequest request, ModelMap model) {
+	public String edit(Integer id,Pageable pageable, HttpServletRequest request, ModelMap model) {
 		model.addAttribute("model", manager.findById(id));
-		model.addAttribute("pageNo", pageNo);
+		model.addAttribute("pageable", pageable);
 		return "admin/questioncatalog/edit";
 	}
 
 	@RequestMapping("/admin/questioncatalog/model_save")
 	public String save(QuestionCatalog bean, HttpServletRequest request, ModelMap model) {
 		bean = manager.save(bean);
+		Integer parid =bean.getParentId();
+
 		log.info("save Teacher id={}", bean.getId());
-		return "redirect:view_list.htm";
-	}
+		if (bean.getParentId() != null) {
+			return "redirect:/admin/questioncatalog/view_list.htm?id=" + bean.getParentId();
+
+		} else {
+			return "redirect:/admin/questioncatalog/view_list.htm";
+
+		}	}
 
 	@RequestMapping("/admin/questioncatalog/model_update")
-	public String update(Pageable pageable, QuestionCatalog bean,HttpServletRequest request, ModelMap model) {
+	public String update(Pageable pageable, QuestionCatalog bean, HttpServletRequest request, ModelMap model) {
 		bean = manager.update(bean);
 		log.info("update Teacher id={}.", bean.getId());
-		return list(pageable, request, model);
+		if (bean.getParentId() != null) {
+			return "redirect:/admin/questioncatalog/view_list.htm?id=" + bean.getParentId();
+
+		} else {
+			return "redirect:/admin/questioncatalog/view_list.htm";
+
+		}
 	}
 
 	@RequestMapping("/admin/questioncatalog/model_delete")
 	public String delete(Pageable pageable, Integer id, HttpServletRequest request, ModelMap model) {
-			 
-				manager.deleteById(id);
-			 
-		return list(pageable, request, model);
+
+		Integer parid = manager.findById(id).getParentId();
+		manager.deleteById(id);
+
+		if (parid != null) {
+			return "redirect:/admin/questioncatalog/view_list.htm?id=" + parid;
+
+		} else {
+			return "redirect:/admin/questioncatalog/view_list.htm";
+
+		}
 	}
+
 	@RequestMapping("/admin/questioncatalog/model_deletes")
 	public String deletes(Pageable pageable, Integer[] ids, HttpServletRequest request, ModelMap model) {
-			 
-				manager.deleteByIds(ids);
-			 
-		return list(pageable, request, model);
+		Integer parid = manager.findById(ids[0]).getParentId();
+
+		manager.deleteByIds(ids);
+
+		if (parid != null) {
+			return "redirect:/admin/questioncatalog/view_list.htm?id=" + parid;
+		} else {
+			return "redirect:/admin/questioncatalog/view_list.htm";
+
+		}
 	}
+
 	@Autowired
 	private QuestionCatalogService manager;
 }
