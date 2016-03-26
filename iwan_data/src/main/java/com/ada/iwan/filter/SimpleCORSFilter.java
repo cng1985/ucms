9939016cbdb8d3d16cedf30c70d5003545fe4c06
ada.young <cng1985@gmail.com>
2016-03-26 23:51:54 +0800

    修改配置文件

--- iwan_data/src/main/java/com/ada/iwan/filter/SimpleCORSFilter.java
+++ iwan_data/src/main/java/com/ada/iwan/filter/SimpleCORSFilter.java
@@ -2,33 +2,25 @@ package com.ada.iwan.filter;
 
 import java.io.IOException;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class SimpleCORSFilter implements Filter {
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-			throws IOException, ServletException {
-		HttpServletResponse response = (HttpServletResponse) res;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class SimpleCORSFilter extends OncePerRequestFilter {
+
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-		try {
-			chain.doFilter(req, res);
-		} catch (Exception e) {
-			e.printStackTrace();
-			response.sendRedirect("/login.htm");
-		}
+		filterChain.doFilter(request, response);
 	}
 
-	public void init(FilterConfig filterConfig) {
-	}
-
-	public void destroy() {
-	}
 }
