df45f3572da35fa2577d33a692d01b2b66febc26
ada.young <cng1985@gmail.com>
2016-05-21 23:27:56 +0800

    测试级联删除

--- iwan_data/src/main/java/com/ada/iwan/apps/UserApps.java
+++ iwan_data/src/main/java/com/ada/iwan/apps/UserApps.java
@@ -8,8 +8,19 @@ public class UserApps {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		UserInfoService service = ObjectFactory.get().getBean(
-				UserInfoService.class);
+		UserInfoService service = ObjectFactory.get().getBean(UserInfoService.class);
+		try {
+			UserInfo user = service.deleteById(1l);
+			System.out.println(user);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getClass().getName());
+		}
+	
+		System.out.println("xxx");
+	}
+
+	private static void x(UserInfoService service) {
 		UserInfo user = service.findById(1l);
 		System.out.println(user);
 		user.getAttributes().put("sex", "男1");
