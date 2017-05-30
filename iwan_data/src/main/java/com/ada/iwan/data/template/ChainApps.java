2a38d9eac350754c3edc6300d5492cd77aff75a0
ada.young <cng1985@gmail.com>
2017-05-30 10:35:03 +0800

    修改分类管理

--- iwan_data/src/main/java/com/ada/iwan/data/template/ChainApps.java
+++ iwan_data/src/main/java/com/ada/iwan/data/template/ChainApps.java
@@ -2,25 +2,31 @@ package com.ada.iwan.data.template;
 
 import java.io.File;
 
+import com.ada.activity.entity.ActivityCatalog;
 import com.ada.article.entity.Article;
+import com.ada.article.entity.ArticleCatalog;
 import com.ada.article.entity.SensitiveCategory;
 import com.ada.data.entity.CatalogEntity;
 import com.ada.imake.ChainMake;
+import com.ada.imake.template.hibernate.TemplateHibernateDir;
+import com.ada.imake.templates.ace.TemplateAceDir;
+import com.ada.imake.templates.simple.TemplateSimpleDir;
 
 public class ChainApps {
 
 	public static void main(String[] args) {
 
 		File file = new File("D:\\mvnspace\\iwan\\iwan_web\\src\\main\\webapp\\WEB-INF\\ftl\\admin");
-		ChainMake make = new ChainMake();
+		ChainMake make = new ChainMake(TemplateSimpleDir.class, TemplateHibernateDir.class);
 		make.setAction("com.ada.iwan.controller.admin");
 		make.setView(file);
 		make.setDao(false);
 		make.setService(false);
 		make.setAction(true);
 		make.setView(true);
+		make.setMenus("1,49,51");
 		// UserOauthToken.
-		 make.makes(Article.class);
+		 make.makes(ArticleCatalog.class);
 		// make.makes(UserOschina.class,UserOauthWeibo.class,UserQQ.class,UserGitHub.class);
 		// new ChainMake().makes("com.ada.imakedemo.data.entity");
 
