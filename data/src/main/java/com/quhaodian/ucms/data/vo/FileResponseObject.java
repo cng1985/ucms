793ff3a461ae3402f50f001e27775cc6deb453d9
ada.young <cng1985@gmail.com>
2018-03-11 11:58:38 +0800

    修改首页

---
+++ data/src/main/java/com/quhaodian/ucms/data/vo/FileResponseObject.java
@@ -0,0 +1,19 @@
+package com.quhaodian.ucms.data.vo;
+
+import com.quhaodian.discover.rest.base.ResponseObject;
+
+/**
+ * Created by cng19 on 2017/9/3.
+ */
+public class FileResponseObject extends ResponseObject {
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
