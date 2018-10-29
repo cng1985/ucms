cffa71047e74125117b26f06a16a3d062ad9d1a5
ada.young <cng1985@gmail.com>
2018-10-29 21:51:40 +0800

    优化结构

---
+++ data/src/main/java/com/quhaodian/ucms/apps/HackApp.java
@@ -0,0 +1,32 @@
+package com.quhaodian.ucms.apps;
+
+import com.haoxuer.discover.generate.utils.RandomValue;
+import jodd.http.HttpRequest;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class HackApp {
+
+  public static void main(String[] args) {
+    x();
+
+
+
+  }
+
+  private static void x() {
+    for (int i = 0; i < 10000; i++) {
+      reg();
+    }
+  }
+
+  private static void reg() {
+    HttpRequest request= HttpRequest.post("https://www.haoxuer.com/register.htm");
+
+    request.form("username",""+ RandomValue.getEmail(5,12));
+    request.form("password",""+RandomValue.getTel());
+    request.form("name",""+RandomValue.getChineseName());
+    request.send().body();
+  }
+}
