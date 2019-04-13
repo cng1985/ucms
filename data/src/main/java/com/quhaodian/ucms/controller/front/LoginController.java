d24ad58dd4933276561729cf3b5505766b5cdeb7
ada.young <cng1985@gmail.com>
2019-04-13 21:19:30 +0800

    更新模块

--- data/src/main/java/com/quhaodian/ucms/controller/front/LoginController.java
+++ data/src/main/java/com/quhaodian/ucms/controller/front/LoginController.java
@@ -34,9 +34,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import sdk.GeetestConfig;
+import sdk.GeetestLib;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 
 /**
  * 登录页
@@ -71,21 +74,69 @@ public class LoginController extends BaseController {
 
 
   @RequestMapping(value = "/register", method = RequestMethod.POST)
-  public String register(String name, String username, String password,
+  public String register(HttpServletRequest request,String name, String username, String password,
                          Model model, RedirectAttributes attributes) {
+    VerifyVo result = new VerifyVo();
+    GeetestLib gtSdk = new GeetestLib(GeetestConfig.getGeetest_id(), GeetestConfig.getGeetest_key(),
+      GeetestConfig.isnewfailback());
 
-    UserRegisterRequest request = new UserRegisterRequest();
-    request.setBindType(BindType.account);
-    request.setNo(username);
-    request.setPassword(password);
-    UserBasicResponse userx = userInfoService.register(request);
-    if (userx.getCode() == 0) {
-      attributes.addAttribute("msg", "注册成功");
-      return "redirect:/login.htm";
+    String challenge = request.getParameter(GeetestLib.fn_geetest_challenge);
+    String validate = request.getParameter(GeetestLib.fn_geetest_validate);
+    String seccode = request.getParameter(GeetestLib.fn_geetest_seccode);
+
+    //从session中获取gt-server状态
+    int gt_server_status_code = (Integer) request.getSession().getAttribute(gtSdk.gtServerStatusSessionKey);
+
+    //从session中获取userid
+    String userid = (String) request.getSession().getAttribute("userid");
+
+    //自定义参数,可选择添加
+    HashMap<String, String> param = new HashMap<String, String>();
+    param.put("user_id", userid); //网站用户id
+    param.put("client_type", "web"); //web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
+    param.put("ip_address", "127.0.0.1"); //传输用户请求验证时所携带的IP
+
+    int gtResult = 0;
+
+    if (gt_server_status_code == 1) {
+      //gt-server正常，向gt-server进行二次验证
+
+      gtResult = gtSdk.enhencedValidateRequest(challenge, validate, seccode, param);
+      System.out.println(gtResult);
     } else {
-      model.addAttribute("msg", userx.getMsg());
+      // gt-server非正常情况下，进行failback模式验证
+
+      System.out.println("failback:use your own server captcha validate");
+      gtResult = gtSdk.failbackValidateRequest(challenge, validate, seccode);
+      System.out.println(gtResult);
+    }
+
+
+    if (gtResult == 1) {
+      // 验证成功
+      result.setStatus("success");
+      result.setVersion(gtSdk.getVersionInfo());
+
+      UserRegisterRequest registerRequest = new UserRegisterRequest();
+      registerRequest.setBindType(BindType.account);
+      registerRequest.setNo(username);
+      registerRequest.setPassword(password);
+      UserBasicResponse userx = userInfoService.register(registerRequest);
+      if (userx.getCode() == 0) {
+        attributes.addAttribute("msg", "注册成功");
+        return "redirect:/login.htm";
+      } else {
+        model.addAttribute("msg", userx.getMsg());
+        return getView("register");
+      }
+    } else {
+      result.setStatus("fail");
+      result.setVersion(gtSdk.getVersionInfo());
+      attributes.addAttribute("msg", "非法操作");
+      // 验证失败
       return getView("register");
     }
+
 
   }
 
